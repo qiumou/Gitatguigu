50255af50bd14f555aa57247dfa127d5527f8130
eclipse_user <eclipse@zking.com>
2018-07-08 19:03:08 +0800

    update Person.java

--- src/main/java/com/atguigu/entity/Person.java
+++ src/main/java/com/atguigu/entity/Person.java
@@ -1,5 +1,26 @@
 package com.atguigu.entity;
 
 public class Person {
+	
+	private Integer Pno;
+	private String Pname;
+	
+	public Person() {}
+
+	public Integer getPno() {
+		return Pno;
+	}
+
+	public String getPname() {
+		return Pname;
+	}
+
+	public void setPno(Integer pno) {
+		Pno = pno;
+	}
+
+	public void setPname(String pname) {
+		Pname = pname;
+	}
 
 }
